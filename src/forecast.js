327b42074e16c0f4903655e3e0e586c4502627aa
hannashapovalova <hannalova1991@gmail.com>
2022-07-10 21:31:06 +0100

    Add Forecast Function and Current Temperature

--- src/forecast.js
+++ src/forecast.js
@@ -50,6 +50,51 @@ let currentDayOfWeeek = days[now.getDay()];
 let dayOfWeek = document.querySelector("#day");
 dayOfWeek.innerHTML = `${currentDayOfWeeek}`;
 
+
+// Forecast
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sun",
+    "Mon",
+    "Tues",
+    "Wed",
+    "Thurs",
+    "Fri",
+    "Sat"
+];
+  return days[day];
+
+}
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weather-forecast");
+  
+  let forecastHTML = `<div class="row forecast">`;
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 5) {
+    forecastHTML = forecastHTML + `<div class="col forecast-block">
+              <h4 class="forecast-day">
+                ${formatDay(forecastDay.dt)}
+              </h4>
+              <img 
+               src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+               alt="Weather icon"
+               class="forecast-icon">
+              <p class="forecast-temperature">
+                ${Math.round(forecastDay.temp.max)}&deg; | ${Math.round(forecastDay.temp.min)}&deg;
+              </p>
+            </div>`;
+    }
+  })
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
 // API
 
 function getApiWeather(city) {
@@ -112,5 +157,27 @@ function showTemperature(response) {
     let showHumidity = document.querySelector("#humidity");
     showHumidity.innerHTML = `${humidity}`;
 
+    let descriction = response.data.weather[0].description;
+    let showDescription = document.querySelector(".description");
+    showDescription.innerHTML = `${descriction}`;
+
+    let sunset = response.data.sys.sunset;
+    let sunsetElement = `${new Date(sunset * 1000).getHours()}:` + `${new Date(sunset * 1000).getMinutes()}`;
+    let showSunset = document.querySelector(".sunset-value");
+    showSunset.innerHTML = `${sunsetElement}`;
+
+    let sunrise = response.data.sys.sunrise;
+    let sunriseElement = `${new Date(sunrise * 1000).getHours()}:` + `${new Date(sunrise * 1000).getMinutes()}`;
+    let showSunrise = document.querySelector(".sunrise-value");
+    showSunrise.innerHTML = `${sunriseElement}`;
+
+    let iconElement = document.querySelector(".main-icon");
+    iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
     getForecast(response.data.coord);
 }
+
